e0c64e183379f4392d1fbab998cdcfddfee6f905
Gursimar Kaur Sodhi <gursimarkaur.sodhi@gmail.com>
2023-08-14 00:13:54 +0530

    refactor

--- script.js
+++ script.js
@@ -1,61 +1,15 @@
-const submitBtn = document.getElementById("submitBtn");
-const displayArea = document.getElementById("displayArea");
-const localStorage = [];
+const submitBtn = document.getElementById("SubmitBtn"); // Note the case-sensitive ID
 const enrolledData = document.getElementById("enrolledData");
 
-function displayEnrolledStudents(){
-    enrolledData.innerHTML="";
-
-    const savedUserData = localStorage.getItem("userData");
-    if(savedUserData){
-        const userData = JSON.parse(savedUserData);
-        const newRow = document.createElement("tr");
-        const imageCell = document.createElement("td");
-        const imageElement = document.createElement("img");
-        imageElement.src = userData.photo;
-        imageElement.alt = "Student Photo";
-        imageCell.appendChild(imageElement);
-
-        const descriptionCell = document.createElement("td");
-        descriptionCell.innerHTML = `
-            <strong>Name:</strong> ${userData.name}<br>
-            <strong>Email:</strong> ${userData.email}<br>
-            <strong>Website:</strong> ${userData.website}<br>
-            <strong>Gender:</strong> ${userData.gender}<br>
-            <strong>Date of Birth:</strong> ${userData.dob}<br>
-            <strong>Address:</strong> ${userData.address}<br>
-            <strong>Skills:</strong> ${userData.skills.join(", ")}
-        `;
-
-        newRow.appendChild(imageCell);
-        newRow.appendChild(descriptionCell);
-        enrolledData.appendChild(newRow);
-    }
-}
-
-/*
-function displayUserData(userData) {
-    const displayHTML = `
-            <h2>Enrolled Student Information</h2>
-            <p><strong>Name:</strong> ${name}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Website:</strong> ${website}</p>
-            <p><strong>Gender:</strong> ${gender}</p>
-            <p><strong>Date of Birth:</strong> ${dob}</p>
-            <p><strong>Address:</strong> ${address}</p>
-            <p><strong>Skills:</strong> ${skills.join(", ")}</p>
-            <p><strong>Photo:</strong> ${photo}</p>
-        `;
-    displayArea.innerHTML = displayHTML;
-}*/
 submitBtn.addEventListener("click", function () {
+    // Get values of form
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const website = document.getElementById("website").value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
     const dob = document.getElementById("dob").value;
     const address = document.getElementById("address").value;
-    const skills = Array.from(document.querySelectorAll('input[name="skill"]:checked')).map(skill => skill.value);
+    const skills = Array.from(document.querySelectorAll('input[name="skills"]:checked')).map(skills => skills.id);
     const photo = document.getElementById("photo").value;
 
     const userData = {
@@ -68,33 +22,42 @@ submitBtn.addEventListener("click", function () {
         skills,
         photo
     };
-    localStorage.setItem("userData", JSON.stringify(userData));
-    displayEnrolledStudents();
+    const savedData = localStorage.getItem("enrolledStudents");    // get saveddata from localstorage
+    let enrolledStudents = savedData ? JSON.parse(savedData) : [];
+    enrolledStudents.push(userData);                             // Add current data
+    localStorage.setItem("enrolledStudents", JSON.stringify(enrolledStudents));         // Store the updated data local storage
+    document.getElementById("RegistrationForm").reset();        // Clear form
+    displayEnrolledStudents(); // Display data input
 });
+function displayEnrolledStudents() {
+    enrolledData.innerHTML = "";
 
+    const savedData = localStorage.getItem("enrolledStudents");
+    if (savedData) {
+        const enrolledStudents = JSON.parse(savedData);
+
+        enrolledStudents.forEach(userData => {
+            const newRow = document.createElement("tr");
+            const imageCell = document.createElement("td");
+            const imageElement = document.createElement("img");
+            imageElement.src = userData.photo;
+            imageElement.alt = "Student Photo";
+            imageCell.appendChild(imageElement);
+
+            const descriptionCell = document.createElement("td");
+            descriptionCell.innerHTML = `
+            <strong>Name:</strong> ${userData.name}<br>
+            <strong>Email:</strong> ${userData.email}<br>
+            <strong>Website:</strong> ${userData.website}<br>
+            <strong>Gender:</strong> ${userData.gender}<br>
+            <strong>Date of Birth:</strong> ${userData.dob}<br>
+            <strong>Address:</strong> ${userData.address}<br>
+            <strong>Skills:</strong> ${userData.skills.join(", ")}<br><br>
+        `;
+            newRow.appendChild(imageCell);
+            newRow.appendChild(descriptionCell);
+            enrolledData.appendChild(newRow);
+        });
+    }
+}
 displayEnrolledStudents();
-/*function displayUserData(userData){
-    const displayHTML = `
-            <h2>Enrolled Student Information</h2>
-            <p><strong>Name:</strong> ${name}</p>
-                <p><strong>Email:</strong> ${email}</p>
-                <p><strong>Website:</strong> ${website}</p>
-                <p><strong>Gender:</strong> ${gender}</p>
-                <p><strong>Date of Birth:</strong> ${dob}</p>
-                <p><strong>Address:</strong> ${address}</p>
-                <p><strong>Skills:</strong> ${skills.join(", ")}</p>
-                <p><strong>Photo:</strong> ${photo}</p>
-            `;
-    displayArea.innerHTML = displayHTML;
-}*/
-const savedUserData = localStorage.getItem("userData");
-if (savedUserData) {
-    const userData = JSON.parse(savedUserData);
-    displayUserData(userData);
-    //submitBtn.click();
-}
-
-function displayRegistrations() {
-    const enrolledTable = document.getElementById("enrolledTable");
-    enrolledTable.innerHTML = ""; // Clear the entire table
-}
