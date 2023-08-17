3a5e537d49f912ae63c569405625ed9d81addadc
Gursimar Kaur Sodhi <gursimarkaur.sodhi@gmail.com>
2023-08-17 22:25:22 +0530

    refactor

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ submitBtn.addEventListener("click", function () {
     const skills = Array.from(document.querySelectorAll('input[name="skills"]:checked')).map(skills => skills.id);
     const photo = document.getElementById("photo").value;
 
+    const photoInput = document.getElementById("photo");
+    const photoFile = photoInput.files[0];
+
+    const photoUrl = photoFile ? URL.createObjectURL(photoFile) : '';
+
     const userData = {
         name,
         email,
@@ -20,9 +25,9 @@ submitBtn.addEventListener("click", function () {
         dob,
         address,
         skills,
-        photo
+        photo: photoUrl
     };
-    
+
     // Retrieve previously saved data from local storage
     const savedData = localStorage.getItem("enrolledStudents");
     let enrolledStudents = savedData ? JSON.parse(savedData) : [];
@@ -50,7 +55,10 @@ function displayEnrolledStudents() {
             const newRow = document.createElement("tr");
             const imageCell = document.createElement("td");
             const imageElement = document.createElement("img");
-            imageCell.innerHTML=`<strong>photo:</strong><br>`
+
+            console.log("userData.photo:", userData.photo);
+
+            imageCell.innerHTML = `<strong>photo:</strong><br>`
             imageElement.src = userData.photo;
             imageElement.alt = "Student Photo";
             imageCell.appendChild(imageElement);
@@ -65,7 +73,6 @@ function displayEnrolledStudents() {
             <strong>Address:</strong> ${userData.address}<br>
             <strong>Skills:</strong> ${userData.skills.join(", ")}<br>
         `;
-
             newRow.appendChild(imageCell);
             newRow.appendChild(descriptionCell);
             enrolledData.appendChild(newRow);
@@ -74,3 +81,90 @@ function displayEnrolledStudents() {
 }
 
 displayEnrolledStudents();
+
+enrolledStudents.forEach(userData => {
+    const submitBtn = document.getElementById("SubmitBtn"); // Note the case-sensitive ID
+    const enrolledData = document.getElementById("enrolledData");
+
+    submitBtn.addEventListener("click", function () {
+        // Get values from the form fields
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const website = document.getElementById("website").value;
+        const gender = document.querySelector('input[name="gender"]:checked').value;
+        const dob = document.getElementById("dob").value;
+        const address = document.getElementById("address").value;
+        const skills = Array.from(document.querySelectorAll('input[name="skills"]:checked')).map(skills => skills.id);
+        const photo = document.getElementById("photo").value;
+
+        const photoInput = document.getElementById("photo");
+        const photoFile = photoInput.files[0];
+
+        const photoUrl = photoFile ? URL.createObjectURL(photoFile) : '';
+
+        const userData = {
+            name,
+            email,
+            website,
+            gender,
+            dob,
+            address,
+            skills,
+            photo: photoUrl
+        };
+
+        // Retrieve previously saved data from local storage
+        const savedData = localStorage.getItem("enrolledStudents");
+        let enrolledStudents = savedData ? JSON.parse(savedData) : [];
+
+        // Add current user data to the array
+        enrolledStudents.push(userData);
+
+        // Store the updated array back in local storage
+        localStorage.setItem("enrolledStudents", JSON.stringify(enrolledStudents));
+
+        // Clear form fields
+        document.getElementById("RegistrationForm").reset();
+
+        // Display enrolled students
+        displayEnrolledStudents();
+    });
+
+    function displayEnrolledStudents() {
+        enrolledData.innerHTML = "";
+
+        const savedData = localStorage.getItem("enrolledStudents");
+        if (savedData) {
+            const enrolledStudents = JSON.parse(savedData);
+            enrolledStudents.forEach(userData => {
+                const newRow = document.createElement("tr");
+                const imageCell = document.createElement("td");
+                const imageElement = document.createElement("img");
+
+                console.log("userData.photo:", userData.photo);
+
+                imageCell.innerHTML = `<strong>photo:</strong><br>`
+                imageElement.src = userData.photo;
+                imageElement.alt = "Student Photo";
+                imageCell.appendChild(imageElement);
+
+                const descriptionCell = document.createElement("td");
+                descriptionCell.innerHTML = `
+            <strong>Name:</strong> ${userData.name}<br>
+            <strong>Email:</strong> ${userData.email}<br>
+            <strong>Website:</strong> ${userData.website}<br>
+            <strong>Gender:</strong> ${userData.gender}<br>
+            <strong>Date of Birth:</strong> ${userData.dob}<br>
+            <strong>Address:</strong> ${userData.address}<br>
+            <strong>Skills:</strong> ${userData.skills.join(", ")}<br>
+        `;
+                newRow.appendChild(imageCell);
+                newRow.appendChild(descriptionCell);
+                enrolledData.appendChild(newRow);
+
+                /*URL.revokeObjectURL(userData.photo);*/
+            });
+        }
+    }
+    displayEnrolledStudents();
+})
